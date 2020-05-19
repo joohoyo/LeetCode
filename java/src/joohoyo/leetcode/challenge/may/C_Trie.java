11bac3287f1b6c043f45e43adf75f6e96dbf5da2
joohoyo <joohoyo@gmail.com>
2020-05-19 11:47:25 +0900

    May LeetCoding Challenge - 14

---
+++ java/src/joohoyo/leetcode/challenge/may/C_Trie.java
@@ -0,0 +1,116 @@
+package joohoyo.leetcode.challenge.may;
+
+// Implement Trie (Prefix Tree)
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3329/
+// gg
+
+public class C_Trie {
+    public static void main(String[] args) {
+        new C_Trie().test();
+        System.out.println();
+    }
+
+    public void test() {
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));   // returns true
+        System.out.println(trie.search("app"));     // returns false
+        System.out.println(trie.startsWith("app")); // returns true
+        trie.insert("app");
+        System.out.println(trie.search("app"));     // returns true
+    }
+
+    class Trie {
+        private TrieNode root;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        /**
+         * Inserts a word into the trie.
+         */
+        public void insert(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char currentChar = word.charAt(i);
+                if (!node.containsKey(currentChar)) {
+                    node.put(currentChar, new TrieNode());
+                }
+                node = node.get(currentChar);
+            }
+            node.setEnd();
+        }
+
+        /**
+         * Returns if the word is in the trie.
+         */
+        public boolean search(String word) {
+            TrieNode node = searchPrefix(word);
+            return node != null && node.isEnd();
+        }
+
+        private TrieNode searchPrefix(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char curLetter = word.charAt(i);
+                if (node.containsKey(curLetter)) {
+                    node = node.get(curLetter);
+                } else {
+                    return null;
+                }
+            }
+            return node;
+        }
+
+        /**
+         * Returns if there is any word in the trie that starts with the given prefix.
+         */
+        public boolean startsWith(String prefix) {
+            TrieNode node = searchPrefix(prefix);
+            return node != null;
+        }
+
+        class TrieNode {
+            private final int R = 26;
+            private TrieNode[] links;
+            private boolean isEnd;
+
+            public TrieNode() {
+                links = new TrieNode[R];
+            }
+
+            public boolean containsKey(char ch) {
+                return links[ch - 'a'] != null;
+            }
+
+            public TrieNode get(char ch) {
+                return links[ch - 'a'];
+            }
+
+            public void put(char ch, TrieNode node) {
+                links[ch - 'a'] = node;
+            }
+
+            public void setEnd() {
+                isEnd = true;
+            }
+
+            public boolean isEnd() {
+                return isEnd;
+            }
+        }
+    }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+}
